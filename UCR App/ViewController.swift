3ef60acb9b1293c4da8e69d96645db9fc6b77b0a
Alan <mathwiz2341@gmail.com>
2023-02-21 23:41:22 -0800

    Initial Commit

---
+++ UCR App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UCR App
+//
+//  Created by Alan Xia on 2/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
